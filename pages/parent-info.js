238e22959effae0892f62bb5ee61524ceee6ee53
Esteban S <esteban.samuelreyes@gmail.com>
2022-03-31 17:54:41 -0500

    improving and changing dates

--- pages/parent-info.js
+++ pages/parent-info.js
@@ -1,8 +1,30 @@
 import { Box, TextField,Card} from '@mui/material';
 import NavBar from '../src/components/navigation/NavBar'
 import * as React from 'react';
+import axios from 'axios';
 
 class ParentInfo extends React.Component{
+  constructor(props){
+    super(props);
+    this.state ={
+      name:null,
+      email:null,
+      celular:null,
+    }
+    this.sendInfo=this.sendInfo.bind(this);
+  }
+  sendInfo(e){
+    e.preventDefault();
+    const data = {
+      Name: this.state.name,
+      Email: this.state.email,
+      Phone: this.state.celular,
+    }
+    axios.post('https://sheet.best/api/sheets/96180dfb-3f29-44ce-b499-a742e9dc2e0a',data).then((response)=>{
+      console.log(response);
+    })
+    console.log(this.state.name,this.state.email,this.state.celular);
+  }
   render(){
     return(
       <div>
@@ -19,6 +41,7 @@ class ParentInfo extends React.Component{
           placeholder="Nombre"
           multiline
           variant="standard"
+          onChange={(e)=> this.setState({name:e.target.value})}
         />
         <TextField
           className='input-data'
@@ -27,6 +50,7 @@ class ParentInfo extends React.Component{
           placeholder="Email"
           multiline
           variant="standard"
+          onChange={(e)=> this.setState({email:e.target.value})}
         />
         <TextField
           className='input-data'
@@ -35,12 +59,15 @@ class ParentInfo extends React.Component{
           placeholder="Celular"
           multiline
           variant="standard"
+          onChange={(e)=> this.setState({celular:e.target.value})}
         />
-        <a
+        <button
+          type='submit'
           className='btn__main-video--cobalt charla'
-          variant="contained">
+          variant="contained"
+          onClick={this.sendInfo}>
             Continuar
-        </a>
+        </button>
         </Card>
       </Box>);
       </div>);
